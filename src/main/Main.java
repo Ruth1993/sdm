1d450c132e0c16b77d7e67d9e5d6cb07478114ed
unknown <ivanlukman93@gmail.com>
2018-10-20 15:32:52 +0200

    test merge

--- src/main/Main.java
+++ src/main/Main.java
@@ -18,7 +18,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("SDM Project");
+		System.out.println("SDM Project.");
 		
 
 		// Test DB connection
