4c3b8a867ef5c5bd94385dba309cd528c70a2429
Ruth1993 <ruth.scholten@gmail.com>
2018-10-22 11:39:38 +0200

    worked on the code, added Policy.java, not finished yet though

--- src/main/Main.java
+++ src/main/Main.java
@@ -36,25 +36,6 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-
-		/**
-		 * All of the below is for testing purposes
-		 */
-		
-		Date birth_date = new Date(1993, 10, 1);
-		List<Role> roles = new ArrayList<Role>();
-		roles.add(new Patient());
-		roles.add(new Employer());
-		
-		Person p1 = new Person(1, "Valentine", birth_date, "Female", "A", roles);
-		Person p2 = new Person(2, "Metin", birth_date, "Male", "B", null);
-		Person p3 = new Person(3, "Ivan", birth_date, "Male", "AB", null);
-		Person p4 = new Person(4, "Ruth", birth_date, "Female", "O", null);
-	
-		System.out.println(p1.patient().test());
-		System.out.println(p1.doctor().test());
-		System.out.println(p1.employer().test());
 
 	}
 
--- src/sdm/Person.java
+++ src/sdm/Person.java
@@ -1,110 +1,13 @@
 package sdm;
 
-import java.util.Date;
-import java.util.List;
-
 import databaseAccess.DBConnection;
 
 public class Person {
 	/** Basic info */
 	private int id;
-	private String name;
-	private Date birth_date;
-	private String gender;
-	private String blood_type;
 	
-	/** Roles 
-	 * These can be either Patient, Doctor or Employer
-	*/
-	private List<Role> roles;
-	
-	public Person(int id, String name, Date birth_date, String gender, String blood_type, List<Role> roles) {
+	public Person(int id) {
 		this.id = id;
-		this.name = name;
-		this.birth_date = birth_date;
-		this.gender = gender;
-		this.blood_type = blood_type;
-		this.roles = roles;
 	}
-	
-	/**
-	 * Add role (patient, doctor or employer) to person
-	 * Method only accessible by authorized institutes
-	 * @require !this.roles.contains(role.getClass())
-	 * @param role
-	 */
-	public void addRole(Role role) {
-		this.roles.add(role);
-	}
-	
-	/**
-	 * Remove role (patient, doctor or employer) from person
-	 * Method only accessible by authorized institutes
-	 * @require this.roles.contains(role.getClass())
-	 * @param role
-	 */
-	public void removeRole(Role role) {
-		this.roles.remove(role);
-	}
-	
-	/**
-	 * Insert or change the name of the emergency contact in the PHR system
-	 * @param emergency_contact
-	 */
-	public void changeEmergencyContactDB(String emergency_contact) {
-		
-		DBConnection.update("UPDATE sdmproject.patients_basic_health_info set emergency_contact= '"+emergency_contact+"' WHERE id_patient= "+this.id+" limit 1");
-		
-	}
-	
-	/**
-	 * Insert or change the id of the family doctor in the PHR system
-	 * @param id_family_doctor
-	 */
-	public void changeIdFamilyDoctorDB(int id_family_doctor) {
-		
-		DBConnection.update("UPDATE sdmproject.patients_basic_health_info set id_family_doctor= '"+
-		id_family_doctor+
-		"' WHERE id_patient= "+this.id+" limit 1");
-		
-	}
-	
-	public Patient patient() {
-		Patient patient = null;
-		
-		for(int i=0; i<this.roles.size(); i++) {
-			Role curRole = this.roles.get(i);
-			if(curRole instanceof Patient) {
-				patient = (Patient) curRole;
-			}
-		}
 
-		return patient;
-	}
-	
-	public Doctor doctor() {
-		Doctor doctor = null;
-		
-		for(int i=0; i<this.roles.size(); i++) {
-			Role curRole = this.roles.get(i);
-			if(curRole instanceof Doctor) {
-				doctor = (Doctor) curRole;
-			}
-		}
-
-		return doctor;
-	}
-	
-	public Employer employer() {
-		Employer employer = null;
-		
-		for(int i=0; i<this.roles.size(); i++) {
-			Role curRole = this.roles.get(i);
-			if(curRole instanceof Employer) {
-				employer = (Employer) curRole;
-			}
-		}
-
-		return employer;
-	}
 }
