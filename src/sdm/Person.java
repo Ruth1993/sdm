8afba0d45e016c9931ebc5f43527091dcaabe645
Ruth1993 <ruth.scholten@gmail.com>
2018-10-25 16:36:01 +0200

    added some more functions to Person

--- src/sdm/Person.java
+++ src/sdm/Person.java
@@ -27,16 +27,72 @@ import databaseAccess.DBConnection;
 import it.unisa.dia.gas.jpbc.Element;
 
 public class Person {
+	/**
+	 * Basic immutable person info
+	 */
 	private int id;
+	private String name;
+	private Date birth_date;
+	private String birth_place;
+	private String gender;
+	private String nationality;
 	
-	private PublicKey PK;
-	private SecretKey SK;
+	/** 
+	 * Attributes a person possesses
+	 * These can be: ..., ..., ...
+	 */
 	private String[] attrs;
 	
-	public Person(int id, String[] attrs) {
+	/**
+	 * Public key and secret key for the CP-ABE encryption
+	 */
+	private PublicKey PK;
+	private SecretKey SK;
+	
+	/**
+	 * Constructor for Person
+	 * @param id
+	 * @param name
+	 * @param birth_date
+	 * @param birth_place
+	 * @param gender
+	 * @param nationality
+	 * @param address
+	 * @param phone_no
+	 * @param basic_info_policy
+	 * @param attrs
+	 */
+	public Person(int id, String name, Date birth_date, String birth_place, String gender, String nationality, String address, String phone_no, String basic_info_policy, String[] attrs) {
 		this.id = id;
+		this.name = name;
+		this.birth_date = birth_date;
+		this.birth_place = birth_place;
+		this.gender = gender;
+		this.nationality = nationality;
+		
 		this.attrs = attrs;
 		
+		this.addPersonsBasicInfoDB(basic_info_policy, name, birth_date, birth_place, gender, nationality, address, phone_no);
+	}
+	/**
+	 * Shorter constructor of Person with default parameters already filled in for more efficient testing purposes
+	 * @param id
+	 * @param name
+	 * @param attrs
+	 */
+	public Person(int id, String name, String[] attrs) {
+		this.id = id;
+		this.name = name;
+		this.birth_date = new Date(25, 10, 1993);
+		this.birth_place = "Enschede";
+		this.gender = "Male";
+		this.nationality = "Dutch";
+		
+		this.attrs = attrs;
+		
+		String default_policy = "READ & (person_company|all_hospital|all_doctor|person_healthclub|person_insurance|person)) | (WRITE & (person)";
+		
+		this.addPersonsBasicInfoDB(default_policy, name, this.birth_date, this.birth_place, this.gender, this.nationality, "Campuslaan 1", "0612345678");
 	}
 	
 	public String[] getAttrs(){
@@ -63,9 +119,30 @@ public class Person {
 		this.SK = SerializeUtils.constructFromByteArray(SecretKey.class, b);
 	}
 	
-	public void changeHealthRecordDB(String emergency_contact, int id_family_doctor) {
-		//to be implemented
-		//sql query
+	/**
+	 * Insert entry into table persons_basic_info according to the person's set policy
+	 * @param name
+	 * @param birth_date
+	 * @param birth_place
+	 * @param gender
+	 * @param nationality
+	 * @param address
+	 * @param phone_no
+	 */
+	public void addPersonsBasicInfoDB(String policy, String name, Date birth_date, String birth_place, String gender, String nationality, String address, String phone_no) {
+		//TODO The values inserted into the database should be the encrypted values according to this person's policy
+		DBConnection.update("INSERT INTO sdmproject.persons_basic_info (id, name, birth_date, birth_place, gender, nationality, address, phone_number) VALUES ("+this.id+", '"+name+"', '"+birth_date+"', '"+birth_place+"', '"+gender+"', '"+nationality+"', '"+ address+"', '"+phone_no+"')");
+	}
+	
+	/**
+	 * Change policy and re-encrypt entries in the database accordingly
+	 * @param policy
+	 * @param address
+	 * @param phone_no
+	 */
+	public void changePolicyPersonsBasicInfoDB(String new_policy, String address, String phone_no) {
+		//TODO change parameters below to the parameters which are encrypted according to the new policy
+		DBConnection.update("UPDATE sdmproject.persons_basic_info SET name='"+this.name+"', birth_date='"+this.birth_date+"', birth_place='"+this.birth_place+"', gender='"+this.gender+"', nationality='"+this.nationality+"', address='"+address+"', phone_number='"+phone_no+"' WHERE id= "+this.id+" limit 1");
 	}
 	
 	/**
